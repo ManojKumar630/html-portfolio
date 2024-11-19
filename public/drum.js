2539bdb2a8688eaa4b70b3cec9434b4512ebbd6a
ManojKumar630 <manojkumarkondaparthii@gmail.com>
2024-11-19 21:30:34 +0530

    Update drum.js

--- public/drum.js
+++ public/drum.js
@@ -15,47 +15,47 @@ for (var i = 0; i < numberOfDrumButton; i++) {
 function makeSound(key){
     switch (key) {
         case "w":
-            var tom1 = new Audio("../sounds/tom-1.mp3");
+            var tom1 = new Audio("../images/tom-1.mp3");
             tom1.play();
-            var tom2 = new Audio("../sounds/tom-2.mp3");
+            var tom2 = new Audio("../images/tom-2.mp3");
             tom2.play();
-            var tom3 = new Audio("../sounds/tom-3.mp3");
+            var tom3 = new Audio("../images/tom-3.mp3");
             tom3.play();
-            var tom4 = new Audio("../sounds/tom-4.mp3");
+            var tom4 = new Audio("../images/tom-4.mp3");
             tom4.play();
-            var snare = new Audio("../sounds/snare.mp3");
+            var snare = new Audio("../images/snare.mp3");
             snare.play();
-            var crash = new Audio("../sounds/crash.mp3");
+            var crash = new Audio("../images/crash.mp3");
             crash.play();
             break;
     
         case "a":
-            var tom2 = new Audio("../sounds/tom-2.mp3");
+            var tom2 = new Audio("../images/tom-2.mp3");
             tom2.play();
             break;
     
         case "s":
-            var tom3 = new Audio("../sounds/tom-3.mp3");
+            var tom3 = new Audio("../images/tom-3.mp3");
             tom3.play();
             break;
     
         case "d":
-            var tom4 = new Audio("../sounds/tom-4.mp3");
+            var tom4 = new Audio("../images/tom-4.mp3");
             tom4.play();
             break;
     
         case "j":
-            var snare = new Audio("../sounds/snare.mp3");
+            var snare = new Audio("../images/snare.mp3");
             snare.play();
             break;
     
         case "k":
-            var crash = new Audio("../sounds/crash.mp3");
+            var crash = new Audio("../images/crash.mp3");
             crash.play();
             break;
     
         case "l":
-            var kick = new Audio("../sounds/kick-bass.mp3");
+            var kick = new Audio("../images/kick-bass.mp3");
             kick.play();
             break;
     
